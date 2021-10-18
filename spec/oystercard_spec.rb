870eabee5e376beaaded46fb7fc89313a99e5930
uburger <ursburger@gmail.com>
2021-10-18 17:07:19 +0200

    05 Add Top Up

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,7 +5,17 @@ describe Oystercard do
     it 'checks if initial balance is zero' do 
       expect(subject.balance).to eq 0
     end
-
   end
+
+  describe '#top_up' do
+    it 'checks if top_up takes argument' do
+      expect(subject).to respond_to(:top_up).with(1).argument
+    end
+
+    it 'tops up the balance' do 
+      expect { subject.top_up(25) }.to change { subject.balance }.by 25
+    end
+  end
+  
 
 end
