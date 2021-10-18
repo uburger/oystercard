f5f321be18d2bdc9c16b5157305ed76bd6971e40
uburger <ursburger@gmail.com>
2021-10-18 16:19:40 +0200

    04_adding_balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,11 @@
 require 'oystercard'
 
 describe Oystercard do 
+  describe '#balance' do 
+    it 'checks if initial balance is zero' do 
+      expect(subject.balance).to eq 0
+    end
+
+  end
 
 end
